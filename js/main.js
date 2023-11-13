d27ae80f6d62c81838cbb1f8c64da022bffb17bb
pyroz7 <vincipyroz7@gmail.com>
2023-11-13 15:42:40 +0100

    hALF

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/* DOBBIAMO CHIEDERE ALL'UTENTE L'ETà E IL NUMERO DI CHILOMETRI CHE VUOLE PERCORRERE */
+
+
+let age = prompt ("Età del passeggero");
+let km  = prompt ("Chilomentri da percorrere");
+alert(prizeTicket)
+
+/* CALCOLARE IL PREZZO DEL BIGLIETTO */
+
+age = parseInt(km);
+let prizeTicket = Math.floor (km * 0.21) ;
+
+/* CALCOLARE IL PREZZO DEL BIGLIETTO IN BASE AGLI SCONTI */
+ 
+if (age < 18) {
+    prizeTicket = prizeTicket * 0.2;
+}
+
+else if (age > 65) {
+    prizeTicket = prizeTicket * 0.4;
+}
+
+/*PREZZO FINALE CON MASSIMO DUE DECINALI*/
+
+let finalPrize= prizeTicket.toFixed(2); 
+
+
+/*STAMP LOG*/ 
+
+console.log("-------------- informazioni Passeggero --------------");
+console.log("> Informazioni personali:" );
+console.log("Età: " + age );
+console.log("Chilometri da percorrere:" + km + "km");
+console.log("Prezzo finale:" + prizeTicket  + "euro" );
+console.log("-------------------------------------------------------------");
